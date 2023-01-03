9957133157f7aac7dca8ca3c64ee4020821ff1cb
kpuwar <kpuwar@student.42heilbronn.de>
2023-01-03 08:49:21 +0100

    minitalk.h updated

--- minitalk.h
+++ minitalk.h
@@ -6,16 +6,14 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 04:06:16 by kpuwar            #+#    #+#             */
-/*   Updated: 2022/12/26 23:24:33 by kpuwar           ###   ########.fr       */
+/*   Updated: 2022/12/30 20:57:55 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <signal.h>
 # include "libft/libft.h"
+# include <signal.h>
 
 #endif
